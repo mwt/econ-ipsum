eeb426d9596d465abadff2b3a15ecf4de89458c7
MattWThomas <macravin@gmail.com>
2020-05-01 13:37:17 -0500

    Reduce API calls * Request paragraphs in blocks of 100 and iterate through on client side * Prevent user from generating 0 paragraphs

--- _includes/main.js
+++ _includes/main.js
@@ -1,27 +1,43 @@
-/* Make warmup request */
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", "https://gfyahgphl9.execute-api.us-east-1.amazonaws.com/Econ-Ipsum?warmup", true);
-xmlHttp.send(null);
+var pcache = [];
+var element = document.getElementById("np");
+var np = 5;
 
-/* Function makes request and puts data in post-content div */
-function xhr() {
-  var tags = document.getElementsByClassName("post-content");
-  var clone = tags[0].cloneNode(false);
+/* Function makes request stores paragraphs in pcache */
+function xhr(post) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhttp.onreadystatechange = function () {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
-      clone.innerHTML = xhttp.responseText;
-      tags[0].parentNode.replaceChild(clone, tags[0]);
+      pcache.push(...xhttp.responseText.split("\n"))
+      if (typeof post !== "undefined") {
+        post();
+      }
     }
   };
-  var element = document.getElementById("np");
-  xhttp.open("GET", "https://gfyahgphl9.execute-api.us-east-1.amazonaws.com/Econ-Ipsum?np=" + element.value, true);
+  xhttp.open("GET", "https://gfyahgphl9.execute-api.us-east-1.amazonaws.com/Econ-Ipsum?np=100", true);
   xhttp.send();
   return;
 };
 
+/* Replace text on page with first `np` elements of pcache */
+function refreshText() {
+  var tags = document.getElementsByClassName("post-content");
+  var clone = tags[0].cloneNode(false);
+  const pused = pcache.splice(0, np);
+  clone.innerHTML = pused.join(" ");
+  tags[0].parentNode.replaceChild(clone, tags[0]);
+  return;
+};
+
 /* Event listener */
-document.getElementById("generate").addEventListener('click', function(e) {
+document.getElementById("generate").addEventListener("click", function (e) {
   e.preventDefault();
-  xhr();
+  np = parseInt(element.value);
+  if (pcache.length >= np) {
+    refreshText();
+  } else if (np <= 100) {
+    xhr(refreshText);
+  }
 }, false);
+
+/* Prefetch */
+xhr();
